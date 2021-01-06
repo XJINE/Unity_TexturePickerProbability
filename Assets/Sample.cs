eac3e9319d6e990831af38f106a4ddeeb5fcc999
XJINE <xjine.andorelseif@gmail.com>
2021-01-06 19:32:13 +0900

    First commit.

---
+++ Assets/Sample.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+
+public class Sample : MonoBehaviour
+{
+    #region Field
+
+    private TextureManager textureManager;
+    private TextureData    pickedTexture;
+
+    #endregion Field
+
+    #region Method
+
+    private void Awake()
+    {
+        textureManager = GetComponent<TextureManager>();
+        textureManager.Initialize();
+
+        pickedTexture = TexturePicker.RandomPick(textureManager);
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            pickedTexture = TexturePickerProbability.Pick(textureManager);
+        }
+    }
+
+    private void OnGUI()
+    {
+        textureManager.Initialize();
+
+        GUILayout.Label("Managed Textures : ");
+
+        foreach (var data in textureManager.Textures)
+        {
+            GUILayout.Label(data.ToString());
+        }
+
+        GUILayout.Label("Press Return to pick random texture with considering probability.");
+        GUILayout.Label("Picked Texture : " + pickedTexture.ToString());
+    }
+
+    #endregion Method
+}
--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # Unity_TexturePickerProbability
+
+TexturePickerProbability regards TextureData.meta.x as probability and pick a texture with considering it.
+
+## Import to Your Project
+
+1. Import dependencies first.
+2. Paste this code into ``PackageManager > Add package from git URL``.
+
+```
+https://github.com/XJINE/Unity_TexturePickerProbability.git?path=/Packages/TexturePickerProbability
+```
+
+### Dependencies
+
+You have to import following assets to use this asset.
+
+- [Unity_TextureManager](https://github.com/XJINE/Unity_TextureManager)
